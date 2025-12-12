bd9f7b4889689f349c757a24d19c57dc67f72614
SaifurSaimon <saimon32kg@gmail.com>
2025-12-12 12:50:18 +0800

    step2 from section 1 created

--- components/ImageToolTip.jsx
+++ components/ImageToolTip.jsx
@@ -12,8 +12,19 @@ const ImageTooltip = ({ src, alt, tooltipText, className }) => {
       <img src={src} alt={alt} className="rounded-lg" />
 
       {show && (
-        <div className="absolute bottom-full mb-2 left-1/2 transform -translate-x-1/2 px-3 py-1 bg-[#8a8a8a] text-white text-sm rounded-lg whitespace-nowrap z-50">
+        <div className="absolute bottom-full mb-3 left-1/2 transform -translate-x-2 px-3 py-1 bg-[#8a8a8a]
+         text-white text-sm whitespace-nowrap z-50 ">
           {tooltipText}
+           <div
+            className="
+              absolute top-full left-3
+              -translate-x-1/2               /* center arrow under tooltip */
+              w-0 h-0 
+              border-l-8 border-l-transparent 
+              border-r-8 border-r-transparent 
+              border-t-8 border-t-[#8a8a8a]  /* arrow color */
+            "
+          />
         </div>
       )}
     </div>
--- components/records/Step2.jsx
+++ components/records/Step2.jsx
@@ -1,9 +1,93 @@
-import React from 'react'
+import React from "react";
+import ImageTooltip from "../ImageToolTip";
+import { useFormContext } from "react-hook-form";
 
 const Step2 = () => {
+  const { register } = useFormContext();
   return (
-    <div>Step2</div>
-  )
-}
+    <div className="mt-9 w-full bg-white p-6">
+      <div>
+        <h2 className="font-medium text-[26px]">1、项目介绍</h2>
+        <div className="flex items-start gap-14 mt-14">
+          <div className="flex items-center w-1/4">
+            <p className="whitespace-nowrap font-medium text-[22px]">
+              开发的背景或原因：
+            </p>
+            <ImageTooltip
+              src="/tooltip.png"
+              alt="Example"
+              tooltipText="（客户为什么要做这个项目？解决什么问题？抓住什么机会？当前业务是如何运作的？痛点在哪里？）"
+            />
+          </div>
+          <textarea
+            className="bg-[#f5f6f8] px-6 py-3.5 rounded-lg w-3/4 h-40"
+            placeholder="请填写开发的背景或原因"
+            {...register("coreNeeds.reason")}
+          />
+        </div>
+        <div className="flex items-start gap-14 mt-6">
+          <div className="flex items-center w-1/4">
+            <p className="whitespace-nowrap font-medium text-[22px]">
+              核心目标：
+            </p>
+            <ImageTooltip
+              src="/tooltip.png"
+              alt="Example"
+              tooltipText="（项目成功上线后，希望达到的具体、可衡量的目标是什么？例如：将订单处理时间缩短30%，将客户转化率提升15%，实现业务全流程线上化等。）"
+            />
+          </div>
+          <textarea
+            className="bg-[#f5f6f8] px-6 py-3.5 rounded-lg w-3/4 h-40"
+            placeholder="请填写开发的核心目标"
+            {...register("coreNeeds.objective")}
+          />
+        </div>
+        <div className="flex items-start gap-14 mt-6">
+          <div className="flex items-center w-1/4">
+            <p className="whitespace-nowrap font-medium text-[22px]">
+              用户目标：
+            </p>
+            <ImageTooltip
+              src="/tooltip.png"
+              alt="Example"
+              tooltipText="（软件给谁用？内部员工、终端消费者、特定商户？请描述用户的基本特征和使用场景。）"
+            />
+          </div>
+          <textarea
+            className="bg-[#f5f6f8] px-6 py-3.5 rounded-lg w-3/4 h-40"
+            placeholder="请填写开发的用户目标"
+            {...register("coreNeeds.userGoal")}
+          />
+        </div>
+      </div>
+      <div className="mt-14">
+        <h2 className="font-medium text-[26px]">2、核心范围与核心功能</h2>
+        <div className="flex items-start gap-14 mt-14">
+          <p className="whitespace-nowrap font-medium text-[22px] w-1/4">
+            参考软件/APP：
+          </p>
 
-export default Step2
+          <input
+            className="bg-[#f5f6f8] px-6 py-3.5 rounded-lg w-3/4"
+            type="text"
+            placeholder="请填写参考软件名称或网址"
+            {...register("coreNeeds.referenceApp")}
+          />
+        </div>
+        <div className="flex items-start gap-14 mt-14">
+          <p className="whitespace-nowrap font-medium text-[22px] w-1/4">
+            项目/产品核心功能：{" "}
+          </p>
+
+          <textarea
+            className="bg-[#f5f6f8] px-6 py-3.5 rounded-lg w-3/4 h-40"
+            placeholder="请填写下项目/产品核心的功能"
+            {...register("coreNeeds.coreFunctions")}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Step2;
